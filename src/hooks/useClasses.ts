9a16a36a491c0b29a4547530deffec88c741be45
Binit Nayak <binit.nayak@arrise.com>
2026-02-08 04:05:51 +0530

    useClasses added

---
+++ src/hooks/useClasses.ts
@@ -0,0 +1,34 @@
+type Modifiers = Record<string, boolean | undefined>;
+
+export const useClasses = (
+  styles: Record<string, string>, // 👈 pass css module object
+  block: string,
+  modifiers?: Modifiers,
+  extra?: string,
+) => {
+  const classes: string[] = [];
+
+  // base class from module
+  if (styles[block]) {
+    classes.push(styles[block]);
+  }
+
+  // modifiers
+  if (modifiers) {
+    Object.entries(modifiers).forEach(([key, value]) => {
+      if (value) {
+        const modKey = `${block}_${key}`; 
+        if (styles[modKey]) {
+          classes.push(styles[modKey]);
+        }
+      }
+    });
+  }
+
+  // external className
+  if (extra) {
+    classes.push(extra);
+  }
+
+  return classes.join(" ");
+};
