00ffc0778e8202b6a79ae6a025c216f387675105
Binit Nayak <binit.nayak@arrise.com>
2026-02-08 04:05:35 +0530

    button added

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,38 @@
+import type { ButtonHTMLAttributes, ReactElement } from "react";
+
+import { useClasses } from "../../hooks/useClasses";
+import { ButtonVariant } from "./type";
+
+import styles from "./Button.module.css";
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  variant: ButtonVariant;
+  glow?: boolean;
+}
+
+export const Button = ({
+  children,
+  variant = ButtonVariant.DEFAULT,
+  glow = false,
+  className,
+  disabled,
+  ...props
+}: ButtonProps): ReactElement => {
+  const mods = useClasses(
+    styles,
+    "button",
+    {
+      neumorphic: variant == ButtonVariant.NEUMORPHIC,
+      glow: glow && !disabled,
+      disabled: disabled,
+    },
+    className,
+  );
+
+  return (
+    <button className={mods} disabled={disabled} {...props}>
+      <span className={styles.button__content}>{children}</span>
+      <span className={styles.button__ripple} />
+    </button>
+  );
+};
