8fee1839378f93a6e251d4edac3c380819d2866e
Binit Nayak <binit.nayak@arrise.com>
2026-02-08 04:06:06 +0530

    temp button visual added

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
+import { Button } from "./components/Button/Button";
+import { ButtonVariant } from "./components/Button/type";
+
 import "./App.css";
 
 export const App = () => {
-  return <>Hello World</>;
+  return (
+    <>
+      {/* TODO:Remove this*/}
+      <Button variant={ButtonVariant.NEUMORPHIC} glow={true} disabled={false}>
+        Neumorphic Glow
+      </Button>
+      <Button variant={ButtonVariant.DEFAULT} glow={true}>
+        Default
+      </Button>
+    </>
+  );
 };
